99861b9af163725194f60fad7791d79fe606bed8
Renatex314 <renatocorte34@gmail.com>
2022-08-15 11:53:37 -0300

    adição da interação com o ângulo beta

--- assets/scripts/code.js
+++ assets/scripts/code.js
@@ -1,11 +1,12 @@
 const root = document.querySelector(':root');
 
-function updateGamma(angle) {
-    root.style.setProperty('--gamma', angle + 'deg');
+function updateAngle(name, angle) {
+    root.style.setProperty(`--${name}`, `${angle}deg`);
 }
 
 function onDeviceOrientation({alpha = 0, beta = 0, gamma = 0}) {
-    updateGamma(-gamma);
+    updateAngle('gamma', -gamma);
+    updateAngle('beta', -beta + 90);
 }
 
 window.addEventListener('deviceorientation', onDeviceOrientation);
